73bfc452c12ff7efc9113c62b3fea14a167e12a5
Diana Muzhylivska <diana.listen@gmail.com>
2026-07-26 20:12:16 +0300

    fix: resolve build issues and finish notehub functionality

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import 'modern-normalize';
 import './index.css';
 import App from './components/App/App';
 
